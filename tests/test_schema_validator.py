a68779f36fcbdefa17dd5b2295e05859d5e91356
grk86 <gv@rstm.dev>
2020-04-03 13:11:00 +0200

    Config schema updated, tests updated

--- tests/test_schema_validator.py
+++ tests/test_schema_validator.py
@@ -19,17 +19,48 @@ class TestSchemaValidator(unittest.TestCase):
         with self.assertRaises(fastjsonschema.exceptions.JsonSchemaException):
             self.schema_validator({'foobar': 1})
 
-    def test_pass(self):
-        payload = {
-            'name': 'test-config',
-            'metadata': {
-                'foobar': 'deadbeef',
-            }
-        }
+    def test_valid_entries_pass(self):
+        entries = [
+            {
+                'name': 'test-config-1',
+                'metadata': {
+                    'foobar': 'deadbeef',
+                },
+            },
+        ]
         try:
-            self.schema_validator(payload)
+            for entry in entries:
+                self.schema_validator(entry)
         except fastjsonschema.exceptions.JsonSchemaException:
             self.fail('This should pass')
+
+    def test_fail_on_non_string_values(self):
+        entries = [
+            '',
+            {},
+            None,
+            12,
+            {
+                'name': 'foobar'
+            },
+            {
+                'metadata': {
+                    'cpu': 'true'
+                }
+            },
+            {
+                'name': 'fail',
+                'metadata': {
+                    'cpu': {
+                        'value': 3.14159
+                    }
+                }
+            }
+        ]
+
+        for entry in entries:
+            with self.assertRaises(fastjsonschema.exceptions.JsonSchemaException):
+                self.schema_validator(entry)
 
 
 if __name__ == '__main__':
