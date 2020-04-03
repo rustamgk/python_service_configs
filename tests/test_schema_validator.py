2bee62dbe08dee08bf8c90f40540fc10f4f36f5d
grk86 <gv@rstm.dev>
2020-04-03 14:45:38 +0200

    Test updated

--- tests/test_schema_validator.py
+++ tests/test_schema_validator.py
@@ -1,45 +1,57 @@
 import typing
 import unittest
 import fastjsonschema
-from webapp.validator import get_schema_validator
+from webapp.validator import get_schema_validator, REGISTRY_ENTRY_SCHEMA
 
 
-class TestSchemaValidator(unittest.TestCase):
+class TestJSONSchemaValidator(unittest.TestCase):
     """
-    TestSchemaValidator
+    TestJSONSchemaValidator
     """
 
     def setUp(self):
-        self.schema_validator = get_schema_validator('schemas/registry-entry-config.schema.json')
+        self.schema_validator = get_schema_validator(REGISTRY_ENTRY_SCHEMA)
 
     def tearDown(self):
         pass
 
-    def test_fail(self):
-        with self.assertRaises(fastjsonschema.exceptions.JsonSchemaException):
-            self.schema_validator({'foobar': 1})
-
     def test_valid_entries_pass(self):
-        entries = [
+        valid_entries = [
             {
                 'name': 'test-config-1',
                 'metadata': {
                     'foobar': 'deadbeef',
                 },
             },
+            {
+                'name': 'datacenter-1',
+                'metadata': {
+                    'monitoring': {
+                        'enabled': 'true'
+                    },
+                    'limits': {
+                        'cpu': {
+                            'enabled': 'false',
+                            'value': '300m'
+                        }
+                    },
+                    'note': 'upgrade required'
+                }
+            }
         ]
         try:
-            for entry in entries:
+            for entry in valid_entries:
                 self.schema_validator(entry)
-        except fastjsonschema.exceptions.JsonSchemaException:
-            self.fail('This should pass')
+        except fastjsonschema.exceptions.JsonSchemaException as exc:
+            self.fail('This should pass; %s' % exc)
 
     def test_fail_on_non_string_values(self):
-        entries = [
+        invalid_entries = [
             '',
             {},
             None,
             12,
+            {'foobar': 1},
             {
                 'name': 'foobar'
             },
@@ -58,7 +70,7 @@ class TestSchemaValidator(unittest.TestCase):
             }
         ]
 
-        for entry in entries:
+        for entry in invalid_entries:
             with self.assertRaises(fastjsonschema.exceptions.JsonSchemaException):
                 self.schema_validator(entry)
 
